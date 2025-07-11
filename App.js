b9bc63e712ea2b03fc871a62b1858f50094763f2
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-07-11 16:05:21 +0200

    Update App.js

--- App.js
+++ App.js
@@ -362,7 +362,12 @@ function App() {
         React.createElement("div", { className: "w-full max-w-md mt-20 mb-10 p-4" },
           React.createElement("div", { className: "bg-white p-8 rounded-lg shadow-xl w-full text-center" },
             React.createElement("h1", { className: "text-3xl font-bold text-gray-800 mb-4" }, "Vitajte na stránke Slovak Open Handball"),
-            React.createElement("p", { className: "text-lg text-gray-600" }, "Prosím, prihláste sa alebo sa zaregistrujte, aby ste mohli pokračovať."),
+            // Podmienené zobrazenie textu
+            user ? (
+              React.createElement("p", { className: "text-lg text-gray-600" }, "Ste prihlásený. Prejdite do svojej zóny pre viac možností.")
+            ) : (
+              React.createElement("p", { className: "text-lg text-gray-600" }, "Prosím, prihláste sa alebo sa zaregistrujte, aby ste mohli pokračovať.")
+            ),
             React.createElement("div", { className: "mt-6 flex justify-center space-x-4" },
               user ? (
                 React.createElement("a", {
