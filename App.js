c8c7369c9567cd7124df4d326b013cff53a50bb9
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-07-17 22:04:52 +0200

    Update App.js

--- App.js
+++ App.js
@@ -112,7 +112,6 @@ function NotificationModal({ message, isVisible, onClose }) {
 
 function App() {
   const RECAPTCHA_SITE_KEY = "6LdJbn8rAAAAAO4C50qXTWva6ePzDlOfYwBDEDwa";
-  // UPDATED: Zmenená URL adresa pre Google Apps Script
   const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzPbN2BL4t9qRxRVmJs2CH6OGex-l-z21lg7_ULUH3249r93GKV_4B_Oenf6ydz0CyKrA/exec"; 
 
   const [app, setApp] = React.useState(null);
