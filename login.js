6d3e7d9c49094cc0ee15fa86537f399ea7534f40
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-07-20 22:46:50 +0200

    Update login.js

--- login.js
+++ login.js
@@ -338,7 +338,7 @@ function App() {
       setLoading(false); 
 
       setTimeout(() => {
-        window.location.href = 'logged-in-my-data.html';
+        window.location.href = 'logged-in-my-data.html'; // ZMENA: Presmerovanie na logged-in-my-data.html
       }, 5000); 
 
     } catch (e) {
@@ -360,7 +360,7 @@ function App() {
   // Ak je user objekt (prihlásený), presmeruj.
   if (user === undefined || loading) {
     if (user) { // Ak je user objekt, znamená to, že bol prihlásený, ale ešte sa načítava
-        window.location.href = 'logged-in-my-data.html'; // Presmerovanie ak je používateľ prihlásený
+        window.location.href = 'logged-in-my-data.html'; // ZMENA: Presmerovanie na logged-in-my-data.html
         return null; // Nič nevykresľuj počas presmerovania
     }
     return React.createElement(
