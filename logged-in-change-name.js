3e5a006fadec1ca6fedec6d96137b8cd4eca76d4
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-07-25 13:17:03 +0200

    Update logged-in-change-name.js

--- logged-in-change-name.js
+++ logged-in-change-name.js
@@ -387,9 +387,9 @@ function ChangeNameApp() {
         window.location.href = 'login.html';
         return null;
     }
-    let loadingMessage = 'Načítavam aplikáciu...';
+    let loadingMessage = 'Načítavam...';
     if (isAuthReady && user && !userProfileData) {
-        loadingMessage = 'Načítavam používateľské dáta...';
+        loadingMessage = 'Načítavam...';
     } else if (loading) {
         loadingMessage = 'Načítavam...';
     }
