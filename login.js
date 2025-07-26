7ca9d6add69a2e2ff4f5090db7829acded081777
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-07-26 09:39:20 +0200

    Update login.js

--- login.js
+++ login.js
@@ -141,12 +141,13 @@ function ResetPasswordModal({ show, onClose, onSendResetEmail, loading, message,
         { className: 'bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4', role: 'alert' },
         error
       ),
-      message && React.createElement(
+      message && React.createElement( // Zelené okno s úspešnou správou
         'div',
         { className: 'bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4', role: 'alert' },
         message
       ),
-      React.createElement(
+      // Podmienené zobrazenie formulára a tlačidiel
+      !message && React.createElement( // Skryť, ak je zobrazená úspešná správa
         'form',
         { onSubmit: handleSubmit },
         React.createElement(
