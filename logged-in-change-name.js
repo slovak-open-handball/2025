29112e61b815c0ce8fd4024f5dfacbd10253e0b5
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-07-28 12:25:19 +0200

    Update logged-in-change-name.js

--- logged-in-change-name.js
+++ logged-in-change-name.js
@@ -4,15 +4,15 @@
 
 // const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwYROR2fU0s4bVri_CTOMOTNeNi4tE0YxeekgtJncr-fPvGCGo3igXJfZlJR4Vq1Gwz4g/exec";
 
-const formatToDatetimeLocal = (date) => {
-  if (!date) return '';
-  const year = date.getFullYear();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  return `${year}-${month}-${day}T${hours}:${minutes}`;
-};
+//const formatToDatetimeLocal = (date) => {
+//  if (!date) return '';
+//  const year = date.getFullYear();
+//  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+//  const day = date.getDate().toString().padStart(2, '0');
+//  const hours = date.getHours().toString().padStart(2, '0');
+//  const minutes = date.getMinutes().toString().padStart(2, '0');
+//  return `${year}-${month}-${day}T${hours}:${minutes}`;
+//};
 
 // NotificationModal Component for displaying temporary messages (converted to React.createElement)
 function NotificationModal({ message, onClose }) {
