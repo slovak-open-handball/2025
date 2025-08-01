6433fd21bc5dea1315bea82d3200438b7a316953
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-01 16:26:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const formatDate = (timestamp) => {
     const year = date.getFullYear();
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
-    return `dňa ${day}.&nbsp;${month}.&nbsp;${year}&nbsp;o&nbsp;${hours}:${minutes}&nbsp;hod`;
+    return `dňa ${day}.&nbsp;${month}.&nbsp;${year}&nbsp;o&nbsp;${hours}:${minutes}&nbsp;hod.`;
 };
 
 /**
