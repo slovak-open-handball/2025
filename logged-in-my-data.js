869a2eefd2059d5a20ab56bc043ac06a7117d91c
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-02 18:44:07 +0200

    Update logged-in-my-data.js

--- logged-in-my-data.js
+++ logged-in-my-data.js
@@ -107,7 +107,7 @@ const MyDataApp = ({ userProfileData, roleColor }) => {
                             d: 'M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z'
                         })
                     ),
-                    'Upraviť profil'
+                    'Upraviť'
                 )
             ),
             // Zmena rozloženia na jeden stĺpec a odstránenie obdĺžnikov
