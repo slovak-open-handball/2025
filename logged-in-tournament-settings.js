0039fda2640a1a65e9510aa9c6487769c227dda8
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-07 20:54:18 +0200

    Update logged-in-tournament-settings.js

--- logged-in-tournament-settings.js
+++ logged-in-tournament-settings.js
@@ -293,7 +293,7 @@ function TournamentSettingsApp() {
 
   return React.createElement(
     'div',
-    { className: 'min-h-screen bg-gray-100 flex flex-col items-start font-inter overflow-y-auto' },
+    { className: 'min-h-screen bg-gray-100 flex flex-col items-center font-inter overflow-y-auto' },
     React.createElement(
       'div',
       { className: 'w-full max-w-2xl p-4' }, // Zmenené: odstránené mt-20 a mb-10
