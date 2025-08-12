a40d5ff2ab5cd3bd873e36bec49e276dbe1c61d2
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-12 17:35:01 +0200

    Update register-page4.js

--- register-page4.js
+++ register-page4.js
@@ -337,8 +337,15 @@ export function Page4Form({ formData, handlePrev, handleSubmit, loading, setLoad
                                         {
                                             type: 'button',
                                             onClick: () => handleAddTshirtRow(categoryName, teamIndex),
-                                            className: `bg-blue-500 hover:bg-blue-700 text-white font-bold w-10 h-10 rounded-full flex items-center justify-center mx-auto mt-4 transition-colors duration-200 focus:outline-none focus:shadow-outline ${team.tshirts.some(t => t.size === '') ? 'bg-white text-blue-500 border border-blue-500 cursor-not-allowed' : ''}`,
-                                            disabled: loading || team.tshirts.some(t => t.size === ''),
+                                            className: `
+                                                font-bold w-10 h-10 rounded-full flex items-center justify-center mx-auto mt-4 
+                                                transition-colors duration-200 focus:outline-none focus:shadow-outline
+                                                ${loading || team.tshirts.some(t => t.size === '') || getAvailableTshirtSizeOptions(team.tshirts).length === 0
+                                                    ? 'bg-white text-blue-500 border border-blue-500 cursor-not-allowed'
+                                                    : 'bg-blue-500 hover:bg-blue-700 text-white'
+                                                }
+                                            `.trim(),
+                                            disabled: loading || team.tshirts.some(t => t.size === '') || getAvailableTshirtSizeOptions(team.tshirts).length === 0,
                                         },
                                         '+'
                                     )
