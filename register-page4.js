d50fffc2543bbf2f39498b9c848c3895ec0fd432
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-12 19:05:56 +0200

    Update register-page4.js

--- register-page4.js
+++ register-page4.js
@@ -274,6 +274,8 @@ export function Page4Form({ formData, handlePrev, handleSubmit, loading, setLoad
                             const teamTshirtDifference = teamRequiredTshirts - teamOrderedTshirts;
 
                             const validationMessageColorClass = teamTshirtDifference > 0 ? 'bg-red-100 text-red-700' : 'bg-green-100 text-green-700';
+                            const validationMessageClasses = classNames('mt-2', 'p-2', 'rounded-lg', 'text-center', 'font-bold', validationMessageColorClass);
+
 
                             return React.createElement(
                                 'div',
@@ -426,10 +428,7 @@ export function Page4Form({ formData, handlePrev, handleSubmit, loading, setLoad
                                 teamTshirtDifference !== 0 && React.createElement(
                                     'div',
                                     {
-                                        className: classNames(
-                                            'mt-2', 'p-2', 'rounded-lg', 'text-center', 'font-bold',
-                                            validationMessageColorClass
-                                        )
+                                        className: validationMessageClasses
                                     },
                                     getPerTeamTshirtValidationMessage(teamTshirtDifference, categoryName, team.teamName)
                                 )
@@ -469,7 +468,7 @@ export function Page4Form({ formData, handlePrev, handleSubmit, loading, setLoad
                             React.createElement('path', { className: 'opacity-75', fill: 'currentColor', d: 'M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z' })
                         ),
                         'Registrujem...'
-                    ) : 'Registrovať'
+                    ) : 'Registrovať sa'
                 )
             )
         )
