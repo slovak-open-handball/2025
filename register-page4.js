4efa810394eab30531ca69439a2fb4b9e72f3b44
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-12 15:01:37 +0200

    Update register-page4.js

--- register-page4.js
+++ register-page4.js
@@ -1,7 +1,7 @@
 // register-page4.js
 // Obsahuje komponenty a logiku pre štvrtú (finálnu) stránku registračného formulára - detaily tímov.
 
-export function Page4Form({ formData, handlePrev, handleSubmit, loading, setLoading, notificationMessage, setShowNotification, setNotificationType, setRegistrationSuccess, isRecaptchaReady, selectedCountryDialCode, NotificationModal, numberOfPlayersLimit, numberOfTeamMembersLimit, teamsDataFromPage4, setTeamsDataFromPage4 }) {
+export function Page4Form({ formData, handlePrev, handleSubmit, loading, setLoading, notificationMessage, setShowNotification, setNotificationType, setRegistrationSuccess, isRecaptchaReady, selectedCountryDialCode, NotificationModal, numberOfPlayersLimit, numberOfTeamMembersLimit, teamsDataFromPage4, setTeamsDataFromPage4, closeNotification }) { // DÔLEŽITÁ OPRAVA: Pridaný closeNotification do destructuringu props
     // formData.categories obsahuje štruktúru: { 'Kategória Názov': { numberOfTeams: X }, ... }
     // Vytvoríme počiatočný stav pre tímy na základe formData.categories a numberOfTeamsFromPage3
     const [teamDetails, setTeamDetails] = React.useState(() => {
