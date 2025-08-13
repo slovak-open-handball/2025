7fb135ce68aac48c6f65edf4c893dca72a1e3380
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-13 13:44:23 +0200

    Update register.js

--- register.js
+++ register.js
@@ -222,7 +222,7 @@ function App() {
 
       const days = Math.floor(difference / (1000 * 60 * 60 * 24));
       const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
     return `${days}d ${hours}h ${minutes}m ${seconds}s`;
@@ -1004,6 +1004,7 @@ function App() {
                   setLoading: setLoading,
                   setRegistrationSuccess: setRegistrationSuccess,
                   handleChange: handleChange,
+                  isRecaptchaReady: isRecaptchaReady, // NOVINKA: Odovzdanie isRecaptchaReady
               }) :
           page === 6 ? // NOVINKA: Renderovanie Page6Form
               React.createElement(Page6Form, {
