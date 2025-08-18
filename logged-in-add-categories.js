98ed050a797e4ede639039b5890c8be15a694937
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-18 19:46:22 +0200

    Update logged-in-add-categories.js

--- logged-in-add-categories.js
+++ logged-in-add-categories.js
@@ -820,7 +820,7 @@ function AddCategoriesApp() {
     }),
     React.createElement(
       'div',
-      { className: 'w-full max-w-3xl p-4' }, {/* Zmenené z max-w-md na max-w-3xl */}
+      { className: 'w-full max-w-3xl p-4' },
       React.createElement(
         'div',
         { className: 'bg-white p-8 rounded-lg shadow-xl w-full' },
