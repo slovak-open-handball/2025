a6a7ff2fd4a104560776d538071faa5462fcdbfd
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-26 14:41:03 +0200

    Update logged-in-left-menu.js

--- logged-in-left-menu.js
+++ logged-in-left-menu.js
@@ -20,6 +20,7 @@ const setupMenuListeners = (userProfileData, db, userId) => {
     const addCategoriesLink = document.getElementById('add-categories-link'); // Získanie odkazu na kategórie
     const tournamentSettingsLink = document.getElementById('tournament-settings-link'); // NOVINKA: Získanie odkazu na nastavenia turnaja
     const allRegistrationsLink = document.getElementById('all-registrations-link'); // NOVINKA: Získanie odkazu na všetky registrácie
+    const mySettingsLink = document.getElementById('my-settings-link'); // NOVINKA: Získanie odkazu na moje nastavenia
     
     if (!leftMenu || !menuToggleButton || menuTexts.length === 0 || !menuSpacer) {
         console.error("left-menu.js: Nepodarilo sa nájsť #left-menu, #menu-toggle-button, textové elementy alebo menu spacer po vložení HTML.");
@@ -69,12 +70,14 @@ const setupMenuListeners = (userProfileData, db, userId) => {
     const showAdminLinks = () => {
         if (userProfileData.role === 'admin') {
             addCategoriesLink.classList.remove('hidden');
-            tournamentSettingsLink.classList.remove('hidden'); // NOVINKA: Zobrazenie odkazu na nastavenia turnaja
-            allRegistrationsLink.classList.remove('hidden'); // NOVINKA: Zobrazenie odkazu na všetky registrácie
+            tournamentSettingsLink.classList.remove('hidden'); 
+            allRegistrationsLink.classList.remove('hidden');
+            mySettingsLink.classList.remove('hidden'); // NOVINKA: Zobrazenie odkazu na moje nastavenia
         } else {
             addCategoriesLink.classList.add('hidden');
-            tournamentSettingsLink.classList.add('hidden'); // NOVINKA: Skrytie odkazu na nastavenia turnaja
-            allRegistrationsLink.classList.add('hidden'); // NOVINKA: Skrytie odkazu na všetky registrácie
+            tournamentSettingsLink.classList.add('hidden');
+            allRegistrationsLink.classList.add('hidden');
+            mySettingsLink.classList.add('hidden'); // NOVINKA: Skrytie odkazu na moje nastavenia
         }
     };    
 
