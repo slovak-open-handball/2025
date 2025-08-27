37d5bb728ede67b53cef3e91b1adc27272eea80e
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-27 18:11:08 +0200

    Update admin-register.js

--- admin-register.js
+++ admin-register.js
@@ -20,8 +20,7 @@ const EyeIcon = React.createElement(
 const EyeOffIcon = React.createElement(
   'svg',
   { className: 'h-5 w-5 text-gray-500', fill: 'none', viewBox: '0 0 24 24', stroke: 'currentColor' },
-  React.createElement('path', { fill: 'currentColor', stroke: 'none', d: 'M15 12a3 3 0 11-6 0 3 3 0 016 0z' }),
-  React.createElement('path', { fill: 'none', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '2', d: 'M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z' }),
+  React.createElement('path', { fill: 'none', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '2', d: 'M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7 1.274-4.057 5.064-7 9.542-7a9.954 9.954 0 012.336.314m-1.127 1.127l-.809.809m-2.883 2.883l-.809.809m3.69-3.69L12 12m1.125-1.125a3 3 0 11-2.25 2.25l2.25-2.25z' }),
   React.createElement('line', { x1: '21', y1: '3', x2: '3', y2: '21', stroke: 'currentColor', strokeWidth: '2' })
 );
 
