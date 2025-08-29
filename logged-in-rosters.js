41e54741f9084f99294556402abd33bb38c579f3
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-29 18:24:05 +0200

    Update logged-in-rosters.js

--- logged-in-rosters.js
+++ logged-in-rosters.js
@@ -1069,8 +1069,7 @@ function AddTeamModal({ show, onClose, onAddTeam, userProfileData, availablePack
                                     color: isAddTshirtButtonDisabled ? roleColor : 'white', 
                                 }
                             },
-                            React.createElement('svg', { className: 'w-5 h-5', fill: 'none', stroke: 'currentColor', viewBox: '0 0 24 24', xmlns: 'http://www.w3.org/2000/svg' }, React.createElement('path', { strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '2', d=
-'M12 6v6m0 0v6m0-6h6m-6 0H6' }))
+                            React.createElement('svg', { className: 'w-5 h-5', fill: 'none', stroke: 'currentColor', viewBox: '0 0 24 24', xmlns: 'http://www.w3.org/2000/svg' }, React.createElement('path', { strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '2', d='M12 6v6m0 0v6m0-6h6m-6 0H6' }))
                         )
                     )
                 ),
