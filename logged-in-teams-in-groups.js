a63df0ca4c43f786a4d4d375497fc0ac5c41fe94
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-08-30 15:32:45 +0200

    Update logged-in-teams-in-groups.js

--- logged-in-teams-in-groups.js
+++ logged-in-teams-in-groups.js
@@ -187,7 +187,7 @@ const AddGroupsApp = ({ userProfileData }) => {
 
         return React.createElement(
             'div',
-            { className: 'flex flex-wrap gap-4' }, // Odstránenie flex-grow a pridanie flex-wrap
+            { className: 'flex flex-wrap lg:flex-nowrap gap-4' },
             sortedCategoryIds.map((categoryId, index) => {
                 const groups = allGroupsByCategoryId[categoryId];
                 const categoryName = categoryIdToNameMap[categoryId] || "Neznáma kategória";
@@ -207,7 +207,7 @@ const AddGroupsApp = ({ userProfileData }) => {
                 
                 return React.createElement(
                     'div',
-                    { key: index, className: 'w-64 flex flex-col bg-white rounded-xl shadow-xl p-8 transform transition-all duration-500 hover:scale-[1.01] mb-6 flex-shrink-0' }, // Pridaná pevná šírka a flex-shrink
+                    { key: index, className: 'w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex flex-col bg-white rounded-xl shadow-xl p-8 transform transition-all duration-500 hover:scale-[1.01] mb-6 flex-shrink-0' }, // Dynamická šírka na základe obrazovky
                     React.createElement(
                         'h3',
                         { className: 'text-2xl font-semibold mb-4 text-center' },
@@ -244,7 +244,7 @@ const AddGroupsApp = ({ userProfileData }) => {
 
     return React.createElement(
         'div',
-        { className: 'flex flex-wrap lg:flex-nowrap justify-center space-x-0 lg:space-x-4 w-full px-4' }, 
+        { className: 'flex flex-col lg:flex-row justify-center space-x-0 lg:space-x-4 w-full px-4' }, 
         React.createElement(
             'div',
             { className: `w-full lg:w-1/3 max-w-sm bg-white rounded-xl shadow-xl p-8 transform transition-all duration-500 hover:scale-[1.01] mb-6 flex-shrink-0` },
