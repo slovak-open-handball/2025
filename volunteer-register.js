9ddfb05a4b4044ba3feb3ba69b2c69ab92a1b033
Slovak Open Handball <miloslav.mihucky@gmail.com>
2025-09-24 17:44:07 +0200

    Update volunteer-register.js

--- volunteer-register.js
+++ volunteer-register.js
@@ -591,7 +591,7 @@ const App = () => {
                             { className: 'h-5 w-5 text-gray-500', fill: 'none', viewBox: '0 0 24 24', stroke: 'currentColor' },
                                 React.createElement('path', { strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '2', d: 'M15 12a3 3 0 11-6 0 3 3 0 016 0z' }),
                                 React.createElement('path', { strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '2', d: 'M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z' })
-                            );
+                            )
                         :
                         React.createElement(
                             'svg',
@@ -599,7 +599,7 @@ const App = () => {
                                 React.createElement('path', { fill: 'currentColor', stroke: 'none', d: 'M15 12a3 3 0 11-6 0 3 3 0 016 0z' }),
                                 React.createElement('path', { fill: 'none', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '2', d: 'M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z' }),
                                 React.createElement('line', { x1: '21', y1: '3', x2: '3', y2: '21', stroke: 'currentColor', strokeWidth: '2' })
-                        );
+                        )
                 )
             ),
             (formData.confirmPassword !== '' && formData.password !== formData.confirmPassword) && React.createElement(
@@ -927,7 +927,7 @@ const App = () => {
             onSelect: handleDialCodeSelect,
             selectedDialCode: selectedDialCode,
             unlockedButtonColor: unlockedButtonColor,
-        }),
+        })
     );
 };
 
