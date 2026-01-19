badb16184d0acb13a3b79f62e2c69fa55c1eaab4
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-01-19 13:36:24 +0100

    Update logged-in-teams-in-groups.js

--- logged-in-teams-in-groups.js
+++ logged-in-teams-in-groups.js
@@ -978,7 +978,7 @@ const AddGroupsApp = (props) => {
             allTeams: allTeams,
             defaultCategoryId: selectedCategoryId,
             defaultGroupName: selectedGroupName,
-        }),
+        })
         ),
         React.createElement(
             'div',
