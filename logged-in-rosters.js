326b9b1a887ad4317af471d8e86c6f3c1904c6e1
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-01-27 12:07:24 +0100

    Update logged-in-rosters.js

--- logged-in-rosters.js
+++ logged-in-rosters.js
@@ -938,7 +938,10 @@ function MemberDetailsModal({
 }
 
 function EditTeamModal({ show, onClose, teamData, onSaveTeam, onDeleteTeam, userProfileData, availablePackages, availableAccommodationTypes, availableTshirtSizes, isDataEditDeadlinePassed }) {
-    const [editedTeamData, setEditedTeamData] = React.useState(null);
+    const [editedTeamData, setEditedTeamData] = React.useState({
+        jerseyHomeColor: '',
+        jerseyAwayColor: ''
+    });
     const [editedTeamName, setEditedTeamName] = useState(teamData ? teamData.teamName : '');
     const [editedCategoryName, setEditedCategoryName] = useState(teamData ? teamData.categoryName : '');
     const [editedArrivalType, setEditedArrivalType] = useState(teamData ? teamData.arrival?.type || 'bez dopravy' : 'bez dopravy');
