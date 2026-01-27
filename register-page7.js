d6c67a3aca4c08a0b4fdcae11a97f560e5d35eb3
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-01-27 10:47:15 +0100

    Update register-page7.js

--- register-page7.js
+++ register-page7.js
@@ -337,23 +337,22 @@ export function Page7Form({ formData, handlePrev, handleSubmit, loading, teamsDa
                     });
 
 
-                    {
-                      team.jerseyColors && (team.jerseyColors.color1 || team.jerseyColors.color2) && (
-                        React.createElement('div', { className: 'text-gray-700 mb-3' },
-                          React.createElement('p', { className: 'font-medium' }, 'Farby dresov:'),
-                          team.jerseyColors.color1 &&
-                            React.createElement('p', { className: 'ml-4' },
-                              'Farba dresov 1: ',
-                              React.createElement('span', { className: 'font-semibold' }, team.jerseyColors.color1)
-                            ),
-                          team.jerseyColors.color2 &&
-                            React.createElement('p', { className: 'ml-4' },
-                              'Farba dresov 2: ',
-                              React.createElement('span', { className: 'font-semibold' }, team.jerseyColors.color2)
+                        team.jerseyColors && (team.jerseyColors.color1 || team.jerseyColors.color2) &&
+                          React.createElement('div', { className: 'text-gray-700 mb-3' },
+                            React.createElement('p', { className: 'font-medium mb-1' }, 'Farby dresov:'),
+                            React.createElement('div', { className: 'space-y-1 ml-4' },
+                              team.jerseyColors.color1 &&
+                                React.createElement('p', { className: 'text-gray-700' },
+                                  'Farba dresov 1: ',
+                                  React.createElement('span', { className: 'font-semibold' }, team.jerseyColors.color1)
+                                ),
+                              team.jerseyColors.color2 &&
+                                React.createElement('p', { className: 'text-gray-700' },
+                                  'Farba dresov 2: ',
+                                  React.createElement('span', { className: 'font-semibold' }, team.jerseyColors.color2)
+                                )
                             )
-                        )
-                      )
-                    }
+                          )
                                                
                         React.createElement('p', { className: 'text-sm text-gray-700 mb-4' }, `Počet hráčov: ${team.players || 0}, členovia realizačného tímu (ženy): ${team.womenTeamMembers || 0}, členovia realizačného tímu (muži): ${team.menTeamMembers || 0}, šoféri (ženy): ${team.arrival?.drivers?.female || 0}, šoféri (muži): ${team.arrival?.drivers?.male || 0}`),
                         
