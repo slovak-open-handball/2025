3e87e25fa9db0670e7b7d9e26e54a3201e202b3c
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-01-30 19:38:33 +0100

    Update logged-in-notifications.js

--- logged-in-notifications.js
+++ logged-in-notifications.js
@@ -803,8 +803,15 @@ function NotificationsApp() {
                 disabled: loading
               }),
               React.createElement('div', {
-                className: 'w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[""] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all',
-                style: { backgroundColor: displayNotifications ? '#47b3ff' : '#e5e7eb'}
+                className: `
+                  w-11 h-6 rounded-full 
+                  peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300
+                  peer-checked:after:translate-x-full peer-checked:after:border-white
+                  after:content-[''] after:absolute after:top-[2px] after:left-[2px]
+                  after:bg-white after:border after:border-gray-300 after:rounded-full
+                  after:h-5 after:w-5 after:transition-all
+                  ${displayNotifications ? 'bg-[#47b3ff]' : 'bg-gray-200'}
+                `
               })
             )
           ),
@@ -823,8 +830,15 @@ function NotificationsApp() {
                 disabled: loading
               }),
               React.createElement('div', {
-                className: 'w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[""] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all',
-                style: { backgroundColor: displayTeamBubbles ? '#47b3ff' : '#e5e7eb'}
+                className: `
+                  w-11 h-6 rounded-full 
+                  peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300
+                  peer-checked:after:translate-x-full peer-checked:after:border-white
+                  after:content-[''] after:absolute after:top-[2px] after:left-[2px]
+                  after:bg-white after:border after:border-gray-300 after:rounded-full
+                  after:h-5 after:w-5 after:transition-all
+                  ${displayTeamBubbles ? 'bg-[#47b3ff]' : 'bg-gray-200'}
+                `
               })
             )
           )
