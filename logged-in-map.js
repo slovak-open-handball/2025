d5b585d718f99ddcdcf7349c42055b92a5a8c661
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-02-02 20:35:36 +0100

    Update logged-in-map.js

--- logged-in-map.js
+++ logged-in-map.js
@@ -1,5 +1,5 @@
 // Importy pre Firebase funkcie
-import { doc, getDoc, onSnapshot, updateDoc, addDoc, collection, Timestamp, deleteDoc, GeoPoint, setDoc }
+import { doc, getDoc, onSnapshot, updateDoc, addDoc, collection, Timestamp, deleteDoc, GeoPoint, setDoc, query, where, getDocs }
   from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 const { useState, useEffect, useRef, useCallback } = React;
