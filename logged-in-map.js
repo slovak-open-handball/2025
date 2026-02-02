9dd7c4cab54d9040650ebed60b663b23b589fd37
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-02-02 20:26:13 +0100

    Update logged-in-map.js

--- logged-in-map.js
+++ logged-in-map.js
@@ -1136,7 +1136,7 @@ const AddGroupsApp = ({ userProfileData }) => {
                         // Tlačidlá
                         React.createElement('div', { className: 'flex justify-end gap-3 mt-6' },
                             React.createElement('button', {
-/                                onClick: cancelAddingPlace,
+                                onClick: cancelAddingPlace,
                                 className: 'px-5 py-2.5 rounded-lg border border-gray-300 text-gray-700 hover:bg-gray-100 transition'
                             }, 'Zrušiť'),
                             
