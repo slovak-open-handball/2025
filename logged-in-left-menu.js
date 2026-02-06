dde0c6f6e55ec9d5e601dc04d18eaf2d1b88f274
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-02-06 19:17:23 +0100

    Update logged-in-left-menu.js

--- logged-in-left-menu.js
+++ logged-in-left-menu.js
@@ -123,7 +123,7 @@ const setupMenuListeners = (userProfileData, db, userId) => {
             teamRostersLink.classList.add('hidden');
             teamsInGroupsLink.classList.remove('hidden');
             mapLink?.classList.remove('hidden');
-            teamsAccommodationLink?.classList.add('hidden');
+            teamsAccommodationLink?.classList.remove('hidden');
 
             const unreadCount = userProfileData.unreadNotificationCount || 0;
             if (notificationsTextWithCount) {
