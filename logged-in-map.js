000a4e7a258697ec91ad029bea73b3a95c5ba73c
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-02-09 19:27:25 +0100

    Update logged-in-map.js

--- logged-in-map.js
+++ logged-in-map.js
@@ -1424,11 +1424,10 @@ const AddGroupsApp = ({ userProfileData }) => {
                 wheelPxPerZoomLevel: 100
               })
               .setView(defaultCenter, defaultZoom)
-              L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-                  attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, © <a href="https://carto.com/attributions">CARTO</a>',
-                  maxZoom: 19,
-                  noWrap: true
-              }).addTo(leafletMap.current);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(leafletMap.current);
             
             // Custom Zoom + Home control
             L.Control.ZoomHome = L.Control.extend({
