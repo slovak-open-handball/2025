b385fe70662478e30575a9bee482ca5aaeb13aaa
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-02-12 13:55:44 +0100

    Update clear-console.js

--- clear-console.js
+++ clear-console.js
@@ -2,60 +2,29 @@ var CLEAR_CONSOLE = 1;
 
 (function() {
     if (CLEAR_CONSOLE === 1) {
-        // Okamžité vyčistenie pri načítaní
-        if (typeof console !== 'undefined' && typeof console.clear === 'function') {
-            console.clear();
-        }
+        // ZACHOVÁME PÔVODNÉ METÓDY
+        var originalLog = console.log;
+        var originalInfo = console.info;
+        var originalWarn = console.warn;
+        var originalError = console.error;
+        var originalDebug = console.debug;
         
-        // Zachytenie a úplné potlačenie všetkých console výpisov
-        if (typeof console !== 'undefined') {
-            // Uložíme pôvodné metódy (pre prípad, že by sme ich chceli neskôr obnoviť)
-            var originalLog = console.log;
-            var originalInfo = console.info;
-            var originalWarn = console.warn;
-            var originalError = console.error;
-            var originalDebug = console.debug;
-            
-            // Prepíšeme console.log - žiadny výpis
-            console.log = function() {
-                // Vyčistíme a NEVYPÍŠEME nič
-                if (CLEAR_CONSOLE === 1) {
-                    console.clear();
-                }
-                // return originalLog.apply(console, arguments); // TOTO SME ODSTRÁNILI
-            };
-            
-            // Prepíšeme console.info - žiadny výpis
-            console.info = function() {
-                if (CLEAR_CONSOLE === 1) {
-                    console.clear();
-                }
-                // ŽIADNY VÝPIS
-            };
-            
-            // Prepíšeme console.warn - žiadny výpis
-            console.warn = function() {
-                if (CLEAR_CONSOLE === 1) {
-                    console.clear();
-                }
-                // ŽIADNY VÝPIS
-            };
-            
-            // Prepíšeme console.error - žiadny výpis
-            console.error = function() {
-                if (CLEAR_CONSOLE === 1) {
-                    console.clear();
-                }
-                // ŽIADNY VÝPIS
-            };
-            
-            // Prepíšeme console.debug - žiadny výpis
-            console.debug = function() {
-                if (CLEAR_CONSOLE === 1) {
-                    console.clear();
-                }
-                // ŽIADNY VÝPIS
-            };
-        }
+        // ÚPLNE POTLAČÍME VŠETKY VÝPISY – BEZ console.clear()
+        console.log = function() {};
+        console.info = function() {};
+        console.warn = function() {};
+        console.error = function() {};
+        console.debug = function() {};
+        
+        // Voliteľné: ticho potlačíme aj groupCollapsed, group, groupEnd
+        if (console.groupCollapsed) console.groupCollapsed = function() {};
+        if (console.group) console.group = function() {};
+        if (console.groupEnd) console.groupEnd = function() {};
+        if (console.table) console.table = function() {};
+        if (console.dir) console.dir = function() {};
+        if (console.time) console.time = function() {};
+        if (console.timeEnd) console.timeEnd = function() {};
+        if (console.trace) console.trace = function() {};
+        if (console.assert) console.assert = function() {};
     }
 })();
