614461dbaf3249bc59a1f46c77fbf20b1eb43942
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-02-12 14:27:55 +0100

    Update logged-in-tournament-settings-category-settings.js

--- logged-in-tournament-settings-category-settings.js
+++ logged-in-tournament-settings-category-settings.js
@@ -306,7 +306,7 @@ export function CategorySettings({ db, userProfileData, showNotification }) {
                         if (catChange.oldValues.drawColor !== catChange.newValues.drawColor) {
                             await createCategorySettingsChangeNotification(
                                 'category_draw_color_updated',
-                                [`Zmena farby pre rozlosovanie v kategórii '${catChange.categoryName}': ${catChange.oldValues.drawColor} → ${catChange.newValues.drawColor}`],
+                                [`Zmena farby pre rozlosovanie v kategórii z '${catChange.categoryName}: ${catChange.oldValues.drawColor}' na '${catChange.newValues.drawColor}'`],
                                 {
                                     categoryId: catChange.categoryId,
                                     categoryName: catChange.categoryName,
@@ -321,7 +321,7 @@ export function CategorySettings({ db, userProfileData, showNotification }) {
                         if (catChange.oldValues.transportColor !== catChange.newValues.transportColor) {
                             await createCategorySettingsChangeNotification(
                                 'category_transport_color_updated',
-                                [`Zmena farby pre dopravu v kategórii '${catChange.categoryName}': ${catChange.oldValues.transportColor} → ${catChange.newValues.transportColor}`],
+                                [`Zmena farby pre dopravu v kategórii z '${catChange.categoryName}: ${catChange.oldValues.transportColor}' na '${catChange.newValues.transportColor}'`],
                                 {
                                     categoryId: catChange.categoryId,
                                     categoryName: catChange.categoryName,
