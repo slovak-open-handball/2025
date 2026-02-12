eabfba4f8b8b9db3d5063e34c7b57203d612f1db
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-02-12 19:30:08 +0100

    Update logged-in-tournament-settings-table-settings.js

--- logged-in-tournament-settings-table-settings.js
+++ logged-in-tournament-settings-table-settings.js
@@ -259,19 +259,6 @@ export function TableSettings({ db, userProfileData, showNotification, sendAdmin
           'div',
           { className: 'text-sm text-gray-500 italic mb-4' },
           'Nie sú nastavené žiadne vlastné podmienky poradia. Tímy budú zoradené iba podľa počtu bodov.'
-        ),
-        
-        // Informačný panel
-        React.createElement(
-          'div',
-          { className: 'bg-blue-50 border border-blue-200 rounded-lg p-4' },
-          React.createElement('div', { className: 'flex' },
-            React.createElement('div', { className: 'flex-shrink-0' },
-              React.createElement('svg', { className: 'h-5 w-5 text-blue-400', viewBox: '0 0 20 20', fill: 'currentColor' },
-                React.createElement('path', { fillRule: 'evenodd', d: 'M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z', clipRule: 'evenodd' })
-              )
-            ),
-          )
         )
       ),
       
