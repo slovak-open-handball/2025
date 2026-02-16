2285e18eede2d5dc00f4fd093b2c69db0ca6926f
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-02-16 20:24:42 +0100

    Update logged-in-matches.js

--- logged-in-matches.js
+++ logged-in-matches.js
@@ -6119,7 +6119,7 @@ const AddMatchesApp = ({ userProfileData }) => {
                                                                                         React.createElement(
                                                                                             'div',
                                                                                             { 
-                                                                                                className: `text-green-600 whitespace-nowrap min-w-[220px] flex-shrink-0`,
+                                                                                                className: `text-green-600 whitespace-nowrap min-w-[230px] flex-shrink-0`,
                                                                                                 style: { textAlign: 'left' }
                                                                                             },
                                                                                             'voľný čas'
@@ -6521,7 +6521,7 @@ const AddMatchesApp = ({ userProfileData }) => {
                                                                                 React.createElement(
                                                                                     'div',
                                                                                     { 
-                                                                                        className: `text-green-600 whitespace-nowrap min-w-[220px] flex-shrink-0`,
+                                                                                        className: `text-green-600 whitespace-nowrap min-w-[230px] flex-shrink-0`,
                                                                                         style: { textAlign: 'left' }
                                                                                     },
                                                                                     'voľný čas'
