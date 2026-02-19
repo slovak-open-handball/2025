99d692dd15ad7c6590d912a84616b9917783e63d
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-02-19 09:33:09 +0100

    Update logged-in-teams-in-groups.js

--- logged-in-teams-in-groups.js
+++ logged-in-teams-in-groups.js
@@ -2166,7 +2166,7 @@ const createTeamAssignmentNotification = async (action, team) => {
                                         title: groupHasMatches 
                                             ? 'Pre túto skupinu už existujú zápasy, nie je možné upravovať tímy' 
                                             : 'Upraviť tím',
-//                                        disabled: groupHasMatches
+                                        disabled: groupHasMatches
                                     },
                                     React.createElement('svg', { className: 'w-5 h-5', fill: 'none', stroke: 'currentColor', viewBox: '0 0 24 24' },
                                         React.createElement('path', { strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '2', d: 'M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z' })
@@ -2185,7 +2185,7 @@ const createTeamAssignmentNotification = async (action, team) => {
                                         title: groupHasMatches 
                                             ? 'Pre túto skupinu už existujú zápasy, nie je možné odstrániť tím' 
                                             : (team.isSuperstructureTeam ? 'Odstrániť tím' : 'Zrušiť zaradenie do skupiny'),
-//                                        disabled: groupHasMatches
+                                        disabled: groupHasMatches
                                     },
                                     React.createElement('svg', { className: 'w-5 h-5', fill: 'none', stroke: 'currentColor', viewBox: '0 0 24 24' },
                                         React.createElement('path', { strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '2', d: 'M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16' })
@@ -2235,7 +2235,7 @@ const createTeamAssignmentNotification = async (action, team) => {
                                         title: groupHasMatches 
                                             ? 'Pre túto skupinu už existujú zápasy, nie je možné upravovať tímy' 
                                             : 'Upraviť tím',
-//                                        disabled: groupHasMatches
+                                        disabled: groupHasMatches
                                     },
                                     React.createElement('svg', { className: 'w-5 h-5', fill: 'none', stroke: 'currentColor', viewBox: '0 0 24 24' },
                                         React.createElement('path', { strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '2', d: 'M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z' })
@@ -2254,7 +2254,7 @@ const createTeamAssignmentNotification = async (action, team) => {
                                         title: groupHasMatches 
                                             ? 'Pre túto skupinu už existujú zápasy, nie je možné odstrániť tím' 
                                             : (team.isSuperstructureTeam ? 'Odstrániť tím' : 'Zrušiť zaradenie do skupiny'),
-//                                        disabled: groupHasMatches
+                                        disabled: groupHasMatches
                                     },
                                     React.createElement('svg', { className: 'w-5 h-5', fill: 'none', stroke: 'currentColor', viewBox: '0 0 24 24' },
                                         React.createElement('path', { strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '2', d: 'M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16' })
