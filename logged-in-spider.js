2e42655d6b2ba9b1091632dcfbcc26d96346f2ff
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-02-19 18:54:04 +0100

    Update logged-in-spider.js

--- logged-in-spider.js
+++ logged-in-spider.js
@@ -1648,7 +1648,7 @@ const SpiderApp = ({ userProfileData }) => {
                                                         justifyContent: 'space-around',
                                                         alignItems: 'center',
                                                         width: '100%',
-                                                        gap: '10px'
+                                                        gap: '-100px'
                                                     }
                                                 },
                                                 // Semifinále 1
