87c2664b14dcc9a66e0fc17a5fa91f052f23ecdb
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-03-06 17:57:22 +0100

    Update logged-in-matches-hall.js

--- logged-in-matches-hall.js
+++ logged-in-matches-hall.js
@@ -2070,19 +2070,19 @@ const matchesHallApp = ({ userProfileData }) => {
                                             case 'yellow':
                                                 eventDisplay = React.createElement(
                                                     'div',
-                                                    { className: 'w-3 h-6 bg-yellow-400 rounded-md' }
+                                                    { className: 'w-4 h-5 bg-yellow-400 rounded-md' }
                                                 );
                                                 break;
                                             case 'red':
                                                 eventDisplay = React.createElement(
                                                     'div',
-                                                    { className: 'w-3 h-6 bg-red-600 rounded-md' }
+                                                    { className: 'w-4 h-5 bg-red-600 rounded-md' }
                                                 );
                                                 break;
                                             case 'blue':
                                                 eventDisplay = React.createElement(
                                                     'div',
-                                                    { className: 'w-3 h-6 bg-blue-600 rounded-md' }
+                                                    { className: 'w-4 h-5 bg-blue-600 rounded-md' }
                                                 );
                                                 break;
                                             case 'exclusion':
