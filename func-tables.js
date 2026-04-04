f69715c9de84058f8cade5364f7e232f646eb06c
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-04-04 18:57:51 +0200

    Update func-tables.js

--- func-tables.js
+++ func-tables.js
@@ -395,7 +395,7 @@
         
         // Informácia o počte odohraných zápasov
         const progressBar = generateProgressBar(table.completionPercentage);
-        console.log(`📋 Zápasy: ${table.completedCount} / ${table.totalMatches} odohraných (${table.completionPercentage}%) ${progressBar}`);
+        console.log(`📋 Zápasy: ${table.completedCount} / ${table.totalMatches} odohraných (${table.completionPercentage}%)`);
         console.log('-'.repeat(120));
         console.log(' '.padEnd(4) + 'TÍM'.padEnd(30) + 'Z'.padEnd(5) + 'V'.padEnd(5) + 'R'.padEnd(5) + 'P'.padEnd(5) + 'Skóre'.padEnd(12) + '+/-'.padEnd(6) + 'Body');
         console.log('-'.repeat(120));
