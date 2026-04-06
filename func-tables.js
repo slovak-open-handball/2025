b985b96d43cd2ac4cf229916ed6a38fb9c356300
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-04-06 19:58:21 +0200

    Update func-tables.js

--- func-tables.js
+++ func-tables.js
@@ -1,5 +1,5 @@
 // ========== GLOBÁLNY PREPÍNAČ PRE LOGOVANIE ==========
-const DEBUG_MODE = true;  // false = vypnuté, true = zapnuté
+const DEBUG_MODE = true;  // false = nevypisuje sa, true = vypisuje sa
 
 // Prepísanie console.log (voliteľné - úplne vypne všetky logy)
 if (!DEBUG_MODE) {
