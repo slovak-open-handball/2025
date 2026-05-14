90287b14d9fd92dab92f9fb318df3ffe785c6ab8
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-05-14 10:17:31 +0200

    Update logged-in-matches.js

--- logged-in-matches.js
+++ logged-in-matches.js
@@ -7359,7 +7359,7 @@ const AddMatchesApp = ({ userProfileData }) => {
                                                                             { 
                                                                                 className: 'grid items-start text-xs',
                                                                                 style: { 
-                                                                                    gridTemplateColumns: '130px 280px 10px 280px 160px 160px',
+                                                                                    gridTemplateColumns: '130px 280px 10px 280px 80px 80px',
                                                                                     width: 'fit-content'
                                                                                 },
                                                                                 onClick: function(e) {
