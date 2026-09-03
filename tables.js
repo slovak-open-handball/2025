09019e5ba6e582a5c37172497b48f0b8ccf5cd89
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-09-03 11:45:47 +0200

    Update tables.js

--- tables.js
+++ tables.js
@@ -672,7 +672,6 @@ const GroupTablesView = ({ matches, categoriesData, groupsData, teamNames, hallN
             'div',
             { className: 'mb-6 text-center' },
             React.createElement('h1', { className: 'text-2xl font-bold text-gray-800' }, 'Tabuľky skupín'),
-            React.createElement('p', { className: 'text-sm text-gray-500 mt-1' }, `${groupTables.length} skupín, ${groupTables.reduce((acc, t) => acc + t.teams.length, 0)} tímov`)
         ),
         
         // Filtre
