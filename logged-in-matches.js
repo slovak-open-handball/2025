fe9aaf69798d9e6450fc6e5801e7416d96188ef3
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-09-05 11:35:10 +0200

    Increase minHeight from 15px to 18px

--- logged-in-matches.js
+++ logged-in-matches.js
@@ -9556,7 +9556,7 @@ const AddMatchesApp = ({ userProfileData }) => {
                                                                                    style: { 
                                                                                        width: '100%',
                                                                                        backgroundColor: 'white',
-                                                                                       minHeight: '15px'
+                                                                                       minHeight: '18px'
                                                                                    }
                                                                                },
                                                                                React.createElement(
