91c76ef827652828d4f9bb9483a4ec511641ae32
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-09-07 18:29:30 +0200

    Decrease maxWidth from 1080px to 1060px

--- teams.js
+++ teams.js
@@ -325,7 +325,7 @@ const TeamsOverviewApp = (props) => {
                 }`,
                 style: { 
                     width: '90%',
-                    maxWidth: '1080px',
+                    maxWidth: '1060px',
                     transition: 'transform 0.3s ease, opacity 0.3s ease',
                     borderRadius: '8px',
                     overflow: 'hidden'
