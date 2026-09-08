7b95ecd00c11b1701eabbbb3b222f19697e88ce6
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-09-08 09:11:31 +0200

    Update logged-in-tournament-settings-pages-settings.js

--- logged-in-tournament-settings-pages-settings.js
+++ logged-in-tournament-settings-pages-settings.js
@@ -15,6 +15,7 @@ function PagesSettings({ db, showNotification, sendAdminNotification }) {
     { id: 'map', label: 'Mapa', defaultVisible: false },
     { id: 'teams-in-groups', label: 'Rozlosovanie tímov do skupín', defaultVisible: false },
     { id: 'matches', label: 'Zápasy', defaultVisible: false },
+    { id: 'rosters', label: 'Súpisky', defaultVisible: false }, // Pridaná stránka Súpisky
   ];
 
   // Pomocná funkcia na zoradenie stránok podľa PAGE_DEFINITIONS
