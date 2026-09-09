32577d98e611fb7673d4e49f2242d7467dc9622a
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-09-09 16:16:09 +0200

    Add useRef and useCallback to React imports

--- statistics.js
+++ statistics.js
@@ -3,7 +3,7 @@ import React from "https://esm.sh/react@18.2.0";
 import ReactDOM from "https://esm.sh/react-dom@18.2.0";
 import { doc, getDoc, onSnapshot, updateDoc, collection, query, getDocs, setDoc, addDoc, serverTimestamp, where } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
-const { useState, useEffect, useMemo } = React;
+const { useState, useEffect, useRef, useCallback, useMemo } = React;
 const listeners = new Set();
 
 // Stabilná notifikácia cez portál
