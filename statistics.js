94804b79b17af42be7a6221759daba9b7fc0bb32
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-09-10 09:16:56 +0200

    Update statistics.js

--- statistics.js
+++ statistics.js
@@ -723,13 +723,6 @@ const TeamStatsCollector = ({ teamName, categoryName, onStatsUpdate }) => {
         if (typeof window.matchTracker?.isDataReady === 'function' && window.matchTracker.isDataReady()) {
             console.log('[TeamStatsCollector] matchTracker je už pripravený pri mount');
             matchTrackerWasReady = true;
-            getDocs(matchesQuery).then(snapshot => {
-                processMatches(snapshot).catch(err => {
-                    console.log('[processMatches pri mount] CHYBA:', err);
-                });
-            }).catch(err => {
-                console.log('[getDocs pri mount] CHYBA:', err);
-            });
         }
         
         return () => {
