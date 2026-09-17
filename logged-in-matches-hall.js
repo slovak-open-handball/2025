3b41c888e4da7ebf7777afd452651a1139b22af1
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-09-17 20:43:45 +0200

    Update logged-in-matches-hall.js

--- logged-in-matches-hall.js
+++ logged-in-matches-hall.js
@@ -1,3 +1,4 @@
+// logged-in-matches-hall.js
 import { collection, getDocs, doc, getDoc, onSnapshot, updateDoc, Timestamp, addDoc, query, where, orderBy, deleteDoc } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
 const { useState, useEffect, useRef } = React;
