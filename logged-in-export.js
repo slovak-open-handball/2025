dcc2fbb4ee28bd75c2c168395cd7b6b9690bd992
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-09-23 10:42:53 +0200

    Add getDocs import to logged-in-export.js

--- logged-in-export.js
+++ logged-in-export.js
@@ -1,5 +1,5 @@
 // logged-in-export.js
-import { doc, getDoc, onSnapshot, updateDoc, addDoc, collection, query } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import { doc, getDoc, getDocs, onSnapshot, updateDoc, addDoc, collection, query } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
 const { useState, useEffect } = React;
