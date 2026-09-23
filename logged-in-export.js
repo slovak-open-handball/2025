0474fd9d9afa96839e81844ebb7dd502b1c44b6a
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-09-23 14:27:30 +0200

    Update logged-in-export.js

--- logged-in-export.js
+++ logged-in-export.js
@@ -441,8 +441,8 @@ const exportMatchesToPdf = async (hallName, matchesByDay, formatDateHeaderFn, fo
     try {
         const rect = element.getBoundingClientRect();
 
-        const cssWidth = rect.width * PDF_ZOOM;
-        const cssHeight = rect.height * PDF_ZOOM;
+        const cssWidth = rect.width;
+        const cssHeight = rect.height;
 
         const canvas = await html2canvasFn(element, {
             scale: scaleToUse,
