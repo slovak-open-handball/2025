1d0d3e2594404c99543af381596bc6fd79282a1a
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-09-24 16:56:20 +0200

    Update logged-in-catering.js

--- logged-in-catering.js
+++ logged-in-catering.js
@@ -1002,7 +1002,7 @@ const cateringApp = ({ userProfileData }) => {
                                       'tr',
                                       {
                                           key: team.id || `${team.uid}-${team.teamName}-${rowIndex}`,
-                                          className: rowIndex % 2 === 0 ? 'bg-white' : 'bg-gray-50',
+                                          className: 'bg-white',
                                       },
                                       React.createElement(
                                           'td',
