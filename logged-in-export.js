0c03885b276c27be70c2ece0cd5eada2b57d5505
Slovak Open Handball <miloslav.mihucky@gmail.com>
2026-09-24 10:52:46 +0200

    Update logged-in-export.js

--- logged-in-export.js
+++ logged-in-export.js
@@ -1165,7 +1165,9 @@ const ExportApp = ({ userProfileData }) => {
                             ),
                             availableGroupTypes.map((type, idx) =>
                                 React.createElement('option', { key: `${type}-${idx}`, value: type }, formatGroupType(type))
-                            )
+                            ),
+                            // Pridaná nová možnosť
+                            React.createElement('option', { key: 'playoff-a-zapasy-o-umiestnenie', value: 'playoff-a-zapasy-o-umiestnenie' }, 'Playoff a zápasy o umiestnenie')
                         )
                     ),
                     React.createElement(
